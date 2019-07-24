d6054d9cd4f014ee8a0588b1d2a334adf405574e
Azargaz <huba310@gmail.com>
2019-07-24 10:50:39 +0200

    First Commit. Setup React+Firebase+MaterialUI dependencies. Created main component called App.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+// Main component is being rendererd to the root id element
+ReactDOM.render(<App/>, document.getElementById('root'));
